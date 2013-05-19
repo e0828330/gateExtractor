af481613dcfadfa033f30948f7f13bb8a32f492c
Adel Gadllah <adel.gadllah@gmail.com>
2013-05-19 16:18:37 +0200

    Implement command line parsing

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import gate.Corpus;
 import gate.Factory;
 import gate.Gate;
+import gnu.getopt.Getopt;
 
 import java.io.File;
 import java.net.URL;
@@ -23,25 +24,85 @@ public class Main {
 
 	private static Logger log = Logger.getLogger(Main.class); 
 	
+	private static void usage(String msg) {
+		if (msg != null) {
+			System.err.println(msg);
+		}
+		System.err.println("Usage:");
+		System.err.println("java -jar gateExtractor.jar -i <inputpath> [-t] [-r -o <outputpath>] [-e]");
+		System.exit(1);
+	}
+	
 	/**
 	 * @param args
 	 * @throws Exception 
 	 */
 	public static void main(String[] args) throws Exception {
+		/* Parse command line arguments */
+		Getopt g = new Getopt("gateExtractor", args, "i:o:tre");
+		g.setOpterr(false);
+		
+		String inputPath = "";
+		String outputPath = "";
+		
+		boolean train = false;
+		boolean eval = false;
+		boolean run = false;
+		
+		int c;
+		String arg;
+		while ((c = g.getopt()) != -1) {
+			System.out.println(c);
+			switch (c) {
+				case 'i':
+					arg = g.getOptarg();
+					if (arg == null || arg.isEmpty()) {
+						usage("Please provide an input path");
+					}
+					inputPath = arg;
+					break;
+				case 'o':
+					outputPath = g.getOptarg();
+					break;
+				case 't':
+					train = true;
+					break;
+				case 'r':
+					run = true;
+					break;
+				case 'e':
+					eval = true;
+					break;
+				case '?':
+				default:
+					usage(null);
+					
+			}
+		}
+		
+		if (args.length == 0 || (!run && !train && !eval)) {
+			usage("Nothing to do.");
+		}
+		
+		if (inputPath == null || inputPath.isEmpty()) {
+			usage("Please provide an input path");
+		}
+		
+		if (run && (outputPath == null || outputPath.isEmpty())) {
+			usage("Please provide an output directory!");
+		}
+
+		
 		/* Initialize GATE */
 		String location = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParent();
 		String resourcesFolder = location + "/resources";
 		Gate.setGateHome(new File(resourcesFolder));
 		Gate.init();
-
-		boolean train = false;
-		boolean eval = false;
 		
 		/* Load Corpus */
 		log.info("Loading Corpus ... ");
 		Corpus corpus = Factory.newCorpus("Training Corpus"); 
-		// TODO -this is hardcoded
-		File directory = new File("/home/linux/Dokumente/Information Retrieval/train/key-gate"); 
+		File directory = new File(inputPath); 
 		URL url = directory.toURI().toURL();
 		corpus.populate(url, null, null, true);
 		log.info("Done loading Corpus!");
@@ -53,25 +114,25 @@ public class Main {
 		pipeline.run(corpus, resourcesFolder);
 
 		/* Train */
-		// TODO: Run when specified on the cmd line
 		if (train) {
 			pipeline = new Trainer();
 			pipeline.run(corpus, resourcesFolder);
 		}
 		
 		/* Apply learned rules */
-		pipeline = new Extractor();
-		pipeline.run(corpus, resourcesFolder);
-
-		ExecutorService executorService = Executors.newFixedThreadPool(20);
-		for (int i = 0; i < corpus.size(); i++) {
-			executorService.execute(new OutputGenerator("/tmp/results/", corpus.get(i)));
+		if (run) {
+			pipeline = new Extractor();
+			pipeline.run(corpus, resourcesFolder);
+	
+			ExecutorService executorService = Executors.newFixedThreadPool(20);
+			for (int i = 0; i < corpus.size(); i++) {
+				executorService.execute(new OutputGenerator(outputPath, corpus.get(i)));
+			}
+			executorService.shutdown();
+			executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
 		}
-		executorService.shutdown();
-		executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
 		
 		/* Evaluate results */
-		// TODO: Run when specified on the cmd line
 		if (eval) {
 			pipeline = new Evaluator();
 			pipeline.run(corpus, resourcesFolder);
